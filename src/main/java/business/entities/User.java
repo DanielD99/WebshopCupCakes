191cbcb6cc5abbe3e92de370181814a1a09bc998
Daniel <anas2635boy@live.dkgit add .git commit first>
2021-11-19 00:03:25 +0100

    opdateret user klassen, samt usermapper og userfacaden så de understøtter navn, tlfnr og balance. Der er dog stadig error ved login

--- src/main/java/business/entities/User.java
+++ src/main/java/business/entities/User.java
@@ -3,18 +3,48 @@ package business.entities;
 public class User
 {
 
-    public User(String email, String password, String role)
+    public User(String email, String password, String role, String phone_number, String name, double balance)
     {
         this.email = email;
         this.password = password;
         this.role = role;
+        this.phone_number = phone_number;
+        this.balance = balance;
+        this.name = name;
     }
 
     private int id; // just used to demo retrieval of autogen keys in UserMapper
     private String email;
     private String password; // Should be hashed and secured
     private String role;
+    private String phone_number;
+    private String name;
+    private double balance;
 
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+   /* public void setBalance(double balance) {
+        this.balance = balance;
+    }
+*/
     public String getEmail()
     {
         return email;
